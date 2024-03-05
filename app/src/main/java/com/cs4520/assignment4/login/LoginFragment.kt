1b6d96bfd73327bdfa3fa19700deb914e99f7976
Dat Ngo <datngonyc@gmail.com>
2024-03-05 18:11:35 -0500

    Added login functionality to fragment view

--- app/src/main/java/com/cs4520/assignment4/login/LoginFragment.kt
+++ app/src/main/java/com/cs4520/assignment4/login/LoginFragment.kt
@@ -6,6 +6,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import com.cs4520.assignment4.R
 import com.cs4520.assignment4.databinding.LoginFragmentBinding
 
 class LoginFragment : Fragment() {
@@ -26,13 +28,21 @@ class LoginFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         viewModel = ViewModelProvider(this).get(LoginViewModel::class.java)
 
+        viewModel.loginSuccess.observe(viewLifecycleOwner) { loginSuccess ->
+            if (loginSuccess) {
+                findNavController().navigate(R.id.action_login_fragment_to_product_list_fragment)
+            }
+        }
 
+        binding.loginButton.setOnClickListener {
+            val username = binding.username.text.toString()
+            val password = binding.password.text.toString()
+            viewModel.login(username, password)
+
+            binding.username.text.clear()
+            binding.password.text.clear()
+        }
     }
-
-
-
-
-
 
 }
 
