27d142f2cde8954587f9f88dd71f5f7973b0d720
Dat Ngo <datngonyc@gmail.com>
2024-03-05 17:56:56 -0500

    Create the view binding in the loginfragment.

--- app/src/main/java/com/cs4520/assignment4/login/LoginFragment.kt
+++ app/src/main/java/com/cs4520/assignment4/login/LoginFragment.kt
@@ -1,6 +1,47 @@
 package com.cs4520.assignment4.login
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.cs4520.assignment4.databinding.LoginFragmentBinding
 
 class LoginFragment : Fragment() {
+    private var _binding: LoginFragmentBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var viewModel: LoginViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = LoginFragmentBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = ViewModelProvider(this).get(LoginViewModel::class.java)
+
+
+    }
+
+
+
+
+
+
 }
+
+
+
+
+
+
+
+
+
+
