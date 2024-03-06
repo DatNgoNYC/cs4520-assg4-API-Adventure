1f2432e2133c8e78348158c325c7c13ae015087f
Dat Ngo <datngonyc@gmail.com>
2024-03-06 18:40:43 -0500

    Create clientapi we will use

--- app/src/main/java/com/cs4520/assignment4/Data/Network/ProductsApiClient.kt
+++ app/src/main/java/com/cs4520/assignment4/Data/Network/ProductsApiClient.kt
@@ -1,6 +1,31 @@
 package com.cs4520.assignment4.Data.Network
 
-object ProductsApiClient {
+import com.cs4520.assignment4.Data.Entities.Product
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+
+
+object RetrofitClient {
     const val BASE_URL: String = "https://kgtttq6tg9.execute-api.us-east-2.amazonaws.com/"
-    const val ENDPOINT: String = "prod/"
+
+    val retrofit: Retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    object ProductsApiClient {
+        val apiService: ApiService by lazy {
+            RetrofitClient.retrofit.create(ApiService::class.java)
+        }
+    }
 }
+
+interface ApiService {
+    @GET("prod/?page={id}")
+    fun getProductListByPage(pageNumber: Int): Call<List<Product>>
+}
+
