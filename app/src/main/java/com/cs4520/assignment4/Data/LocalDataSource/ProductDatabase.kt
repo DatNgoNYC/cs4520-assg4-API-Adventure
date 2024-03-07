f383eee9733ec68134f56969ec9602cb5ab84ca8
Dat Ngo <datngonyc@gmail.com>
2024-03-06 21:08:02 -0500

    create singleton pattern for database to initialize it for use

--- app/src/main/java/com/cs4520/assignment4/Data/LocalDataSource/ProductDatabase.kt
+++ app/src/main/java/com/cs4520/assignment4/Data/LocalDataSource/ProductDatabase.kt
@@ -1,6 +1,8 @@
 package com.cs4520.assignment4.Data.LocalDataSource;
 
+import android.content.Context
 import androidx.room.Database;
+import androidx.room.Room
 import androidx.room.RoomDatabase;
 import com.cs4520.assignment4.Data.Entities.Product;
 
@@ -8,4 +10,18 @@ import com.cs4520.assignment4.Data.Entities.Product;
 abstract class ProductDatabase : RoomDatabase() {
     // Define an abstract method to get the DAO for the Product entity
     abstract fun productDao(): ProductDAO
+
+    companion object {
+        private var INSTANCE: ProductDatabase? = null
+        fun getDatabase(context: Context): ProductDatabase {
+            if (INSTANCE == null) {
+                synchronized(this) {
+                    INSTANCE =
+                        Room.databaseBuilder(context,ProductDatabase::class.java, "contact_database")
+                            .build()
+                }
+            }
+            return INSTANCE!!
+        }
+    }
 }
