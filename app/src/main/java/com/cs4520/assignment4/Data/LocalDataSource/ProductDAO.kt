6a181f1a063447ac2e244c421badf93f11b73e98
Dat Ngo <datngonyc@gmail.com>
2024-03-06 19:31:29 -0500

    Update insert operation to accept a list and keep only unique values.

--- app/src/main/java/com/cs4520/assignment4/Data/LocalDataSource/ProductDAO.kt
+++ app/src/main/java/com/cs4520/assignment4/Data/LocalDataSource/ProductDAO.kt
@@ -2,11 +2,16 @@ package com.cs4520.assignment4.Data.LocalDataSource
 
 import androidx.lifecycle.LiveData
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.cs4520.assignment4.Data.Entities.Product
 
 @Dao
 interface ProductDAO {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAll(product: List<Product>)
+
     @Query("Select * FROM products")
     fun getAllProducts(): LiveData<List<Product>>
 }
